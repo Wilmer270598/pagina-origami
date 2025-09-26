5c545a9a8ef8e6026235d4129a251913341f4747
wilmer <origami.18.wmca@gmail.com>
2025-09-25 18:58:23 -0600

    subiendo version1 de pagina

---
+++ javascript.js
@@ -0,0 +1,8 @@
+function mostrarMensaje() {
+  Swal.fire({
+    title: "¡Bienvenido al mundo del origami! 🎉",
+    text: "Muy pronto estarás creando tus propias figuras.",
+    icon: "success",
+    confirmButtonText: "Genial"
+  });
+}
